dc6b6ed46286d6d36a823945d057de4b91887b88
FacuRSar <facursarmiento@gmail.com>
2025-10-27 18:46:19 -0300

    Jumpfinished

--- Hunter Quest/Assets/Scripts/PlayerMovement.cs
+++ Hunter Quest/Assets/Scripts/PlayerMovement.cs
@@ -6,36 +6,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private int speed;
     private Rigidbody2D rb;
     private float jumpForce = 10f;
-    private Transform groundCheck;
+    [SerializeField] private Transform groundCheck;
     private float groundCheckRadius = 0.2f;
     [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
-    private bool isGrounded()
-    {
-        bool Grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        
-        return Grounded;
-    }
-    private void jump()
-    {
-        if (isGrounded())
-        {
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-        }
-    }
-
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         float horizontal = Input.GetAxisRaw("Horizontal");
-        //float vertical = Input.GetAxisRaw("Vertical");
-        //transform.Translate(0, vertical * Time.deltaTime * speed, 0);
+        float vertical = Input.GetAxisRaw("Vertical");
+        transform.Translate(0, vertical * Time.deltaTime * speed, 0);
         transform.Translate(horizontal * Time.deltaTime * speed, 0,0);
-        if (Input.GetButtonDown("Jump"))
-            jump();
+        if (Input.GetButtonDown("Jump")&&isGrounded)
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 }
